2e923329c800a373e3ba8cdcde8975eb936dd123
kyattonippu <139440750+kyattonippu@users.noreply.github.com>
2023-07-25 23:30:45 +0800

    Update Main.java

--- src/main/java/com/kyattonippu/Main.java
+++ src/main/java/com/kyattonippu/Main.java
@@ -2,9 +2,7 @@ package com.kyattonippu;
 
 public class Main {
 
-    private static String msg = "Hello qa.guru!";
-
-    public static void main(String... args) {
-        System.out.println(msg);
+    public static void main(String[] args) {
+        System.out.println("Hello qa.guru!!!!!!!!");
     }
 }
