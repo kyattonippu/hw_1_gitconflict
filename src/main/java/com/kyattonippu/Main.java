f4f9be8aad32205ce95b9c70d20ddb89584d166e
Maya S <kyattonippu@gmail.com>
2023-07-25 23:50:27 +0800

    added getMessage() method

--- src/main/java/com/kyattonippu/Main.java
+++ src/main/java/com/kyattonippu/Main.java
@@ -2,9 +2,10 @@ package com.kyattonippu;
 
 public class Main {
 
-    private static String msg = "Hello qa.guru!";
-
-    public static void main(String... args) {
-        System.out.println(msg);
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+    private static String getMessage() {
+        return "Hello qa.guru!";
     }
 }
